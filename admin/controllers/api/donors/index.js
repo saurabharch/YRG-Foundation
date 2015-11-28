5594dfbf2409f04244016934228c93d65218e7e6
Aravind Srivatsan <asrivatsan@paypal.com>
2015-11-29 00:29:52 +0530

    :mailbox: POST method for donors

--- admin/controllers/api/donors/index.js
+++ admin/controllers/api/donors/index.js
@@ -11,4 +11,43 @@ module.exports = function(router) {
             } : data);
         });
     });
+
+    router.post('/', function(req, res) {
+        var response = {};
+        if (typeof req.body.name === 'undefined' || req.body.name === '') {
+            response = {
+                'error': 'Incorrect params',
+                'message': 'Name cannot be empty'
+            };
+        }
+        if (typeof req.body.email === 'undefined' || req.body.email === '') {
+            response = {
+                'error': 'Incorrect params',
+                'message': 'Email cannot be empty'
+            };
+        }
+        if (typeof req.body.phone === 'undefined' || req.body.phone === '') {
+            response = {
+                'error': 'Incorrect params',
+                'message': 'Phone cannot be empty'
+            };
+        }
+        if (!Object.keys(response).length) {
+            var model = {
+                id: shortid.generate(),
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone
+            };
+
+            DonorModel(model).save(function(err, data) {
+                res.json(err ? {
+                    'error': 'API Error',
+                    'message': 'Could not add donor'
+                } : data);
+            });
+        } else {
+            res.json(response);
+        }
+    });
 };
