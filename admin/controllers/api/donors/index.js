46d6aa3c18acd0ed15c3a4868a094144f9ce49a8
Anubhav Chaturvedi <mailforanubhav@gmail.com>
2015-11-29 03:01:30 +0530

    changed response.send to respond.json

--- admin/controllers/api/donors/index.js
+++ admin/controllers/api/donors/index.js
@@ -5,7 +5,7 @@ var DonorModel = require('../../../models').Donor;
 module.exports = function(router) {
     router.get('/', function(req, res) {
         DonorModel.find(function(err, data) {
-            res.send(err ? {
+            res.json(err ? {
                 'error': 'API Error',
                 'message': 'Error occurred'
             } : data);
--- admin/controllers/api/meals/index.js
+++ admin/controllers/api/meals/index.js
@@ -5,7 +5,7 @@ var MealsModel = require('../../../models').Meals;
 module.exports = function(router) {
     router.get('/', function(req, res) {
         MealsModel.find(function(err, data) {
-            res.send(err ? {
+            res.json(err ? {
                 'error': 'API Error',
                 'message': 'Error occurred'
             } : data);
@@ -14,7 +14,7 @@ module.exports = function(router) {
 
     router.post('/', function(req, res){
         if ( !(req.body.id && req.body.name && req.body.price && req.body.contents) )
-            return res.status(400).send({'message' : 'Invalid request format'});
+            return res.status(400).json({'message' : 'Invalid request format'});
 
         var meal = new MealsModel({
             'id' : Number(req.body.id),
@@ -25,9 +25,9 @@ module.exports = function(router) {
 
         meal.save(function(err){
             if(err)
-                res.status(500).send({'message':'DB insertion error'});
+                res.status(500).json({'message':'DB insertion error'});
             else
-                res.status(200).send({'message':'Success'});
+                res.status(200).json({'message':'Success'});
         });
     });
 };
