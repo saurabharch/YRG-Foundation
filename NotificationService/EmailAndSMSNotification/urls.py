c1fa687359c337b4c9b0b3aca770934a79fc3595
Aravind Srivatsan <asrivatsan@paypal.com>
2015-11-29 05:22:37 +0530

    Revert "Notification Service Added"

--- NotificationService/EmailAndSMSNotification/urls.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'abchauhan'
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^SMS/$', views.index, name='index'),
-    url(r'^email/$', views.email, name='email'),
-]
